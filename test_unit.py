076b836475ab30bbde49e7c10bed90d0de24a690
Dzhanybek Zakiriiaev <dzhanybek.zakiriiaev@gmail.com>
2025-10-14 19:05:49 -0400

    Added a unit test file to be worked with + added pre-commit to requirements

---
+++ test_unit.py
@@ -0,0 +1,15 @@
+"""
+Basic test file for pytest
+Add your test functions below
+"""
+
+import pytest
+
+# def test_your_function_name():
+# Your test code here
+# assert something == expected_value
+
+# Add your test functions here
+# Example:
+# def test_example():
+#     assert True
